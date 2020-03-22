d09e2c96d6d9bceb8b4d49c7a661ed81f47710cf
Chris <c.nottingham@auckland.ac.nz>
2020-03-23 11:31:01 +1300

    add text encoding

---
+++ text_encoding/text_encoding.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+NLP using Python: 
+    Word tokenization
+    Word Embeddings
+"""
+
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
+
+
